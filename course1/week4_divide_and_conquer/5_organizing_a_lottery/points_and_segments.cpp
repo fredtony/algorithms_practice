e839f2d78fcebfddef2dff9058ae8e1c72e40ff2
fredtony <tony.schroeder@gmail.com>
2018-07-23 10:42:30 -0700

    Completed counting segments covering each point. Finished nearest neighbors of a list of points but still debugging. Getting segmentation fault when more than 2 points.

--- course1/week4_divide_and_conquer/5_organizing_a_lottery/points_and_segments.cpp
+++ course1/week4_divide_and_conquer/5_organizing_a_lottery/points_and_segments.cpp
@@ -1,38 +1,80 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
-using std::vector;
+using namespace std;
+
+int binary_search_mod(const vector<int> &a, int x, int left=0, int right=-1) {
+    if (right == -1)
+        right = (int)a.size()-1;
+    if (right - left == 0) {
+        if (x >= a[right])
+            return right;
+        else
+            return right-1;
+    }
+    else if (right - left == 1) {
+        if (x >= a[right])
+            return right;
+        if (x >= a[left])
+            return left;
+        else
+            return left-1;
+    }
+    else {
+        int elem = left + (right - left) / 2;
+        if (a[elem] == x)
+            return elem;
+        else if (x < a[elem])
+            return binary_search_mod(a, x, left=left, right=elem-1);
+        else
+            return binary_search_mod(a, x, left=elem+1, right=right);
+    }
+    
+}
 
 vector<int> fast_count_segments(vector<int> starts, vector<int> ends, vector<int> points) {
-  vector<int> cnt(points.size());
-  //write your code here
-  return cnt;
+    int len = (int)points.size();
+    vector<int> cnt(len);
+    sort(starts.begin(), starts.end());
+    sort(ends.begin(), ends.end());
+    int l, r;
+    for (int i = 0; i < len; ++i) {
+        l = binary_search_mod(starts, points[i]) + 1;
+        r = binary_search_mod(ends, points[i]);
+        if (points[i] != ends[r-1] or r == -1)
+            r++;
+        cnt[i] = l - r;
+    }
+    return cnt;
 }
 
 vector<int> naive_count_segments(vector<int> starts, vector<int> ends, vector<int> points) {
-  vector<int> cnt(points.size());
-  for (size_t i = 0; i < points.size(); i++) {
-    for (size_t j = 0; j < starts.size(); j++) {
-      cnt[i] += starts[j] <= points[i] && points[i] <= ends[j];
+    vector<int> cnt(points.size());
+    for (size_t i = 0; i < points.size(); i++) {
+        for (size_t j = 0; j < starts.size(); j++) {
+            cnt[i] += starts[j] <= points[i] && points[i] <= ends[j];
+        }
     }
-  }
-  return cnt;
+    return cnt;
 }
 
 int main() {
-  int n, m;
-  std::cin >> n >> m;
-  vector<int> starts(n), ends(n);
-  for (size_t i = 0; i < starts.size(); i++) {
-    std::cin >> starts[i] >> ends[i];
-  }
-  vector<int> points(m);
-  for (size_t i = 0; i < points.size(); i++) {
-    std::cin >> points[i];
-  }
-  //use fast_count_segments
-  vector<int> cnt = naive_count_segments(starts, ends, points);
-  for (size_t i = 0; i < cnt.size(); i++) {
-    std::cout << cnt[i] << ' ';
-  }
+    int n, m;
+    cin >> n >> m;
+    vector<int> starts(n), ends(n);
+    for (size_t i = 0; i < starts.size(); i++) {
+        cin >> starts[i] >> ends[i];
+    }
+    vector<int> points(m);
+    for (size_t i = 0; i < points.size(); i++) {
+        cin >> points[i];
+    }
+    //use fast_count_segments
+    // vector<int> cnt = naive_count_segments(starts, ends, points);
+    vector<int> cnt = fast_count_segments(starts, ends, points);
+    for (size_t i = 0; i < cnt.size(); i++) {
+        cout << cnt[i] << ' ';
+    }
 }
+
--- course1/week4_divide_and_conquer/6_closest_points/closest.cpp
+++ course1/week4_divide_and_conquer/6_closest_points/closest.cpp
@@ -5,26 +5,51 @@
 #include <vector>
 #include <string>
 #include <cmath>
+#include <cstdlib>
 
-using std::vector;
-using std::string;
-using std::pair;
-using std::min;
+using namespace std;
 
+struct point {
+    int x, y;
+};
 
-double minimal_distance(vector<int> x, vector<int> y) {
-  //write your code here
-  return 0.;
+float dist(point p0, point p1) {
+    return sqrt(pow((float)(p0.x - p1.x), 2.) + pow((float)(p0.y - p1.y), 2.));
+}
+
+double minimal_distance(vector<point> &points, int l, int r) {
+    cout << l << '/' << r << endl;
+    int gap = r-l;
+    if (gap < 2) return HUGE_VALF;
+    if (gap == 2)  {
+        float d = dist(points[l], points[l+1]);
+        cout << ' ' << d << endl;
+        return d;
+    }
+    int mid = (int)points.size() / 2;
+    float d = fmin(minimal_distance(points, l, mid), minimal_distance(points, mid, r));
+    vector<point> y_sorted(gap);
+    for (int i=l; i<r; i++) {
+        y_sorted.push_back(points[i]);
+    }
+    sort(y_sorted.begin(), y_sorted.end(), [](point const &a, point const &b) -> bool {return a.y < b.y;});
+    for (int i=l; i<r-1; ++i){
+        int end = min(r, i+8);
+        for (int j=i+1; j<end; ++i) {
+            d = fmin(d, dist(y_sorted[i], y_sorted[j]));
+        }
+    }
+    return d;
 }
 
 int main() {
-  size_t n;
-  std::cin >> n;
-  vector<int> x(n);
-  vector<int> y(n);
-  for (size_t i = 0; i < n; i++) {
-    std::cin >> x[i] >> y[i];
-  }
-  std::cout << std::fixed;
-  std::cout << std::setprecision(9) << minimal_distance(x, y) << "\n";
+    size_t n;
+    cin >> n;
+    vector<point> points(n);
+    for (size_t i = 0; i < n; i++) {
+        cin >> points[i].x >> points[i].y;
+    }
+    sort(points.begin(), points.end(), [](point const &a, point const &b) -> bool {return a.x < b.x;});
+    cout << fixed;
+    cout << setprecision(9) << minimal_distance(points, 0, points.size()) << '\n';
 }
