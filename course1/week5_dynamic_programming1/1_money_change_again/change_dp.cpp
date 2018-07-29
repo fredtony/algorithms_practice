e4a1b3c940183e0cad4c6b89ea056c76587c951c
fredtony <tony.schroeder@gmail.com>
2018-07-29 13:14:40 -0700

    Completed change_dp and primitive_calculator. Working properly.

--- course1/week5_dynamic_programming1/1_money_change_again/change_dp.cpp
+++ course1/week5_dynamic_programming1/1_money_change_again/change_dp.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
 
 int get_change(int m) {
-  //write your code here
-  return m / 4;
+	vector<int> change(m+4, m+4);
+	change[0] = 0;
+	for (int i=0; i<m; ++i) {
+		change[i+1] = min(change[i+1], change[i]+1);
+		change[i+3] = min(change[i+3], change[i]+1);
+		change[i+4] = min(change[i+4], change[i]+1);
+	}
+	return change[m];
 }
 
 int main() {
-  int m;
-  std::cin >> m;
-  std::cout << get_change(m) << '\n';
+	int m;
+	cin >> m;
+	cout << get_change(m) << '\n';
 }
--- course1/week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.cpp
+++ course1/week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.cpp
@@ -2,30 +2,38 @@
 #include <vector>
 #include <algorithm>
 
-using std::vector;
+using namespace std;
 
 vector<int> optimal_sequence(int n) {
-  std::vector<int> sequence;
-  while (n >= 1) {
-    sequence.push_back(n);
-    if (n % 3 == 0) {
-      n /= 3;
-    } else if (n % 2 == 0) {
-      n /= 2;
-    } else {
-      n = n - 1;
+    vector<int> sequence, calc(n+1, n+1);
+    calc[0] = 0;
+    for (int i=0; i<n; ++i) {
+        calc[i+1] = min(calc[i+1], calc[i] + 1);
+        if (2*i <= n)
+            calc[2*i] = min(calc[2*i], calc[i] + 1);
+        if (3*i <= n)
+            calc[3*i] = min(calc[3*i], calc[i] + 1);
     }
-  }
-  reverse(sequence.begin(), sequence.end());
-  return sequence;
+    sequence.push_back(n);
+    while (n > 1) {
+        if (calc[n] == calc[n-1]+1)
+            n--;
+        else if ((n%2 == 0) && (calc[n] == calc[n/2]+1))
+            n /= 2;
+        else
+            n /= 3;
+        sequence.push_back(n);
+    }
+    reverse(sequence.begin(), sequence.end());
+    return sequence;
 }
 
 int main() {
-  int n;
-  std::cin >> n;
-  vector<int> sequence = optimal_sequence(n);
-  std::cout << sequence.size() - 1 << std::endl;
-  for (size_t i = 0; i < sequence.size(); ++i) {
-    std::cout << sequence[i] << " ";
-  }
+    int n;
+    cin >> n;
+    vector<int> sequence = optimal_sequence(n);
+    cout << sequence.size() - 1 << endl;
+    for (size_t i = 0; i < sequence.size(); ++i) {
+        cout << sequence[i] << " ";
+    }
 }
