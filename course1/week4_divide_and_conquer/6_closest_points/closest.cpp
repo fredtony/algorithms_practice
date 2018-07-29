d031a3cfb7f7298d54b2f1a8264ef892ddea604c
fredtony <tony.schroeder@gmail.com>
2018-07-29 08:22:04 -0700

    Screwed up indices to various arrays I was using in closes_points. Corrected and tested.

--- course1/week4_divide_and_conquer/6_closest_points/closest.cpp
+++ course1/week4_divide_and_conquer/6_closest_points/closest.cpp
@@ -10,33 +10,47 @@
 using namespace std;
 
 struct point {
-    int x, y;
+    double x, y;
 };
 
-float dist(point p0, point p1) {
-    return sqrt(pow((float)(p0.x - p1.x), 2.) + pow((float)(p0.y - p1.y), 2.));
+double dist(point p0, point p1) {
+    return sqrt(pow((p0.x - p1.x), 2.) + pow((p0.y - p1.y), 2.));
 }
 
 double minimal_distance(vector<point> &points, int l, int r) {
-    cout << l << '/' << r << endl;
     int gap = r-l;
     if (gap < 2) return HUGE_VALF;
     if (gap == 2)  {
-        float d = dist(points[l], points[l+1]);
-        cout << ' ' << d << endl;
+        double d = dist(points[l], points[l+1]);
         return d;
     }
-    int mid = (int)points.size() / 2;
-    float d = fmin(minimal_distance(points, l, mid), minimal_distance(points, mid, r));
-    vector<point> y_sorted(gap);
-    for (int i=l; i<r; i++) {
-        y_sorted.push_back(points[i]);
+    int mid = l + gap / 2;
+    double d = fmin(minimal_distance(points, l, mid), minimal_distance(points, mid, r));
+    point left_point; left_point.x = points[mid].x - d;
+    int left_elem = (int)(lower_bound(points.begin()+l, points.begin()+r, left_point,
+                                      [](point const &a, point const &b) -> bool {return a.x < b.x;})-points.begin());
+    point right_point; right_point.x = points[mid].x + d;
+    int right_elem = (int)(upper_bound(points.begin()+l, points.begin()+r, right_point,
+                                       [](point const &a, point const &b) -> bool {return a.x < b.x;})-points.begin());
+
+    if (left_elem >= right_elem-1) {
+        return d;
+    }
+    vector<point> y_sorted(right_elem-left_elem);
+    if (left_elem >= right_elem) {
+        return d;
+    }
+    for (int i=left_elem; i<right_elem; i++) {
+        int j = i - left_elem;
+        point pt; pt.x = points[i].x; pt.y = points[i].y;
+        y_sorted[j] = pt;
     }
     sort(y_sorted.begin(), y_sorted.end(), [](point const &a, point const &b) -> bool {return a.y < b.y;});
-    for (int i=l; i<r-1; ++i){
-        int end = min(r, i+8);
-        for (int j=i+1; j<end; ++i) {
-            d = fmin(d, dist(y_sorted[i], y_sorted[j]));
+    for (int i=0; i<(int)y_sorted.size()-1; ++i){
+        int end = min((int)y_sorted.size(), i+8);
+        for (int j=i+1; j<end; ++j) {
+            double new_d = dist(y_sorted[i], y_sorted[j]);
+            d = fmin(d, new_d);
         }
     }
     return d;
