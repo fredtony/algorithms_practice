87f4d1b9c65134951fafdefafeda43a8704ce36b
fredtony <tony.schroeder@gmail.com>
2018-07-31 17:21:13 -0700

    Completed bracket matching problem. No bugs detected in tests.

--- course2/week1/check_brackets_in_code/check_brackets.cpp
+++ course2/week1/check_brackets_in_code/check_brackets.cpp
@@ -2,6 +2,8 @@
 #include <stack>
 #include <string>
 
+using namespace std;
+
 struct Bracket {
     Bracket(char type, int position):
         type(type),
@@ -23,23 +25,33 @@ struct Bracket {
 };
 
 int main() {
-    std::string text;
-    getline(std::cin, text);
+    string text;
+    getline(cin, text);
 
-    std::stack <Bracket> opening_brackets_stack;
+    stack <Bracket> opening_brackets_stack;
     for (int position = 0; position < text.length(); ++position) {
         char next = text[position];
 
         if (next == '(' || next == '[' || next == '{') {
-            // Process opening bracket, write your code here
+            opening_brackets_stack.push(Bracket(next, position));
         }
 
-        if (next == ')' || next == ']' || next == '}') {
-            // Process closing bracket, write your code here
+        else if (next == ')' || next == ']' || next == '}') {
+            if (opening_brackets_stack.empty()) {
+                cout << position + 1;
+                return 0;
+            }
+            else if (opening_brackets_stack.top().Matchc(next))
+                opening_brackets_stack.pop();
+            else {
+                cout << position + 1;
+                return 0;
+            }
         }
     }
-
-    // Printing answer, write your code here
-
+    if (opening_brackets_stack.empty())
+        cout << "Success";
+    else
+        cout << opening_brackets_stack.top().position + 1;
     return 0;
 }
