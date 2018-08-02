710636f31073e5e34d85bbf268d6d6dc88dd1ad7
fredtony <tony.schroeder@gmail.com>
2018-08-02 01:07:38 -0700

    Completed build heap from random array problem. No errors detected in testing.

--- course2/week2/make_heap/build_heap.cpp
+++ course2/week2/make_heap/build_heap.cpp
@@ -10,54 +10,61 @@ using std::pair;
 using std::make_pair;
 
 class HeapBuilder {
- private:
-  vector<int> data_;
-  vector< pair<int, int> > swaps_;
+private:
+	vector<int> data_;
+	vector< pair<int, int> > swaps_;
 
-  void WriteResponse() const {
-    cout << swaps_.size() << "\n";
-    for (int i = 0; i < swaps_.size(); ++i) {
-      cout << swaps_[i].first << " " << swaps_[i].second << "\n";
-    }
-  }
+	void WriteResponse() const {
+		cout << swaps_.size() << "\n";
+		for (int i = 0; i < swaps_.size(); ++i) {
+			cout << swaps_[i].first << " " << swaps_[i].second << "\n";
+		}
+	}
 
-  void ReadData() {
-    int n;
-    cin >> n;
-    data_.resize(n);
-    for(int i = 0; i < n; ++i)
-      cin >> data_[i];
-  }
-
-  void GenerateSwaps() {
-    swaps_.clear();
-    // The following naive implementation just sorts 
-    // the given sequence using selection sort algorithm
-    // and saves the resulting sequence of swaps.
-    // This turns the given array into a heap, 
-    // but in the worst case gives a quadratic number of swaps.
-    //
-    // TODO: replace by a more efficient implementation
-    for (int i = 0; i < data_.size(); ++i)
-      for (int j = i + 1; j < data_.size(); ++j) {
-        if (data_[i] > data_[j]) {
-          swap(data_[i], data_[j]);
-          swaps_.push_back(make_pair(i, j));
+	void ReadData() {
+		int n;
+		cin >> n;
+		data_.resize(n);
+		for(int i = 0; i < n; ++i)
+			cin >> data_[i];
+	}
+    
+    void SiftDown(int parent) {
+        int min_idx, left_child = parent * 2 + 1;
+        if (left_child >= data_.size())
+            return;
+        else {
+    		int right_child = left_child + 1;
+    		if (right_child == data_.size())
+    		    min_idx = left_child;
+    		else
+    		    min_idx = (data_[left_child] < data_[right_child] ? left_child : right_child);
+    		if (data_[min_idx] < data_[parent]) {
+    			swaps_.push_back(pair<int,int>(parent, min_idx));
+    			swap(data_[min_idx], data_[parent]);
+    			SiftDown(min_idx);
+    		}
         }
-      }
-  }
+    }
+    
+	void GenerateSwaps() {
+		swaps_.clear();
+		for (int i = (data_.size() / 2) - 1; i >= 0; --i) {
+			SiftDown(i);
+		}
+	}
 
- public:
-  void Solve() {
-    ReadData();
-    GenerateSwaps();
-    WriteResponse();
-  }
+public:
+	void Solve() {
+		ReadData();
+		GenerateSwaps();
+		WriteResponse();
+	}
 };
 
 int main() {
-  std::ios_base::sync_with_stdio(false);
-  HeapBuilder heap_builder;
-  heap_builder.Solve();
-  return 0;
+	std::ios_base::sync_with_stdio(false);
+	HeapBuilder heap_builder;
+	heap_builder.Solve();
+	return 0;
 }
