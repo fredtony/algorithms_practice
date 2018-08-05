36ed8a707f991c93829adb9a084f5872db16c07e
fredtony <tony.schroeder@gmail.com>
2018-08-05 09:28:04 -0700

    Completed job queue problem with priority queue. No bugs detected in testing.

--- course2/week2/job_queue/job_queue.cpp
+++ course2/week2/job_queue/job_queue.cpp
@@ -1,62 +1,78 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <queue>
+#include <tuple>
 
 using std::vector;
 using std::cin;
 using std::cout;
+using std::priority_queue;
+using std::greater;
+
+struct queue_elem {
+    long long tim;
+    int worker;
+};
+
+bool operator >(const queue_elem& x, const queue_elem& y) {
+    return std::tie(x.tim, x.worker) > std::tie(y.tim, y.worker);
+}
 
 class JobQueue {
- private:
-  int num_workers_;
-  vector<int> jobs_;
+private:
+    int num_workers_;
+    vector<int> jobs_;
 
-  vector<int> assigned_workers_;
-  vector<long long> start_times_;
+    vector<int> assigned_workers_;
+    vector<long long> start_times_;
 
-  void WriteResponse() const {
-    for (int i = 0; i < jobs_.size(); ++i) {
-      cout << assigned_workers_[i] << " " << start_times_[i] << "\n";
+    void WriteResponse() const {
+        for (int i = 0; i < jobs_.size(); ++i) {
+            cout << assigned_workers_[i] << " " << start_times_[i] << "\n";
+        }
     }
-  }
 
-  void ReadData() {
+    void ReadData() {
     int m;
     cin >> num_workers_ >> m;
     jobs_.resize(m);
     for(int i = 0; i < m; ++i)
-      cin >> jobs_[i];
-  }
-
-  void AssignJobs() {
-    // TODO: replace this code with a faster algorithm.
-    assigned_workers_.resize(jobs_.size());
-    start_times_.resize(jobs_.size());
-    vector<long long> next_free_time(num_workers_, 0);
-    for (int i = 0; i < jobs_.size(); ++i) {
-      int duration = jobs_[i];
-      int next_worker = 0;
-      for (int j = 0; j < num_workers_; ++j) {
-        if (next_free_time[j] < next_free_time[next_worker])
-          next_worker = j;
-      }
-      assigned_workers_[i] = next_worker;
-      start_times_[i] = next_free_time[next_worker];
-      next_free_time[next_worker] += duration;
+        cin >> jobs_[i];
     }
-  }
 
- public:
-  void Solve() {
+    void AssignJobs() {
+        assigned_workers_.resize(jobs_.size());
+        start_times_.resize(jobs_.size());
+        priority_queue<long long, vector<queue_elem>, greater<queue_elem>> next_free_time;
+        int min_check = std::min(num_workers_, (int)jobs_.size());
+        queue_elem current_elem;
+        for (int i = 0; i < min_check; ++i) {
+            assigned_workers_[i] = i;
+            start_times_[i] = 0;
+            next_free_time.push((queue_elem){jobs_[i], i});
+        }
+        for (int i = num_workers_; i < jobs_.size(); ++i) {
+            current_elem = (queue_elem){next_free_time.top().tim, next_free_time.top().worker};
+            next_free_time.pop();
+            assigned_workers_[i] = current_elem.worker;
+            start_times_[i] = current_elem.tim;
+            current_elem.tim += (long long)jobs_[i];
+            next_free_time.push((queue_elem){current_elem.tim, current_elem.worker});
+        }
+    }
+
+public:
+    void Solve() {
     ReadData();
     AssignJobs();
     WriteResponse();
-  }
+    }
 };
 
 int main() {
-  std::ios_base::sync_with_stdio(false);
-  JobQueue job_queue;
-  job_queue.Solve();
-  return 0;
+    std::ios_base::sync_with_stdio(false);
+    JobQueue job_queue;
+    job_queue.Solve();
+    return 0;
 }
