3b78434a47a34142bf756865f68cdd34dae2d93e
fredtony <tony.schroeder@gmail.com>
2018-08-11 09:07:55 -0700

    Completed substring match problem by implementing Robin-Karp algorithm. No errors detected in testing.

--- course2/week3/hash_substring/hash_substring.cpp
+++ course2/week3/hash_substring/hash_substring.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <iterator>
+#include <cmath>
 
 using std::string;
+using std::cout;
+using std::cin;
+using std::vector;
 typedef unsigned long long ull;
 
 struct Data {
@@ -11,22 +16,44 @@ struct Data {
 
 Data read_input() {
     Data data;
-    std::cin >> data.pattern >> data.text;
+    cin >> data.pattern >> data.text;
     return data;
 }
 
-void print_occurrences(const std::vector<int>& output) {
-    for (size_t i = 0; i < output.size(); ++i)
-        std::cout << output[i] << " ";
-    std::cout << "\n";
+void print_occurrences(const vector<int>& output) {
+    for (int i = (int)output.size()-1; i >= 0; --i)
+        cout << output[i] << " ";
+    cout << "\n";
 }
 
-std::vector<int> get_occurrences(const Data& input) {
+vector<int> get_occurrences(const Data& input) {
     const string& s = input.pattern, t = input.text;
-    std::vector<int> ans;
-    for (size_t i = 0; i + s.size() <= t.size(); ++i)
-        if (t.substr(i, s.size()) == s)
-            ans.push_back(i);
+    static const ull prime = 1000000007;
+    vector<int> ans;
+    ull pattern_hash = 0, input_hash = 0;
+    for (int i = (int)s.size() - 1; i >= 0; --i) {
+        pattern_hash = (pattern_hash * 26) % prime;
+        pattern_hash = (pattern_hash + (ull)(s[i] - 'A')) % prime;
+    }
+    // cout << "pattern hash: " << pattern_hash << std::endl;
+    for (int i = (int)t.size() - 1; i >= (int)(t.size() - s.size()); --i) {
+        input_hash = (input_hash * 26) % prime;
+        input_hash = (input_hash + (ull)(t[i] - 'A')) % prime;
+    }
+    // cout << "text hash: " << input_hash << std::endl;
+    if (pattern_hash == input_hash) {
+        if (t.compare(t.size() - s.size(), s.size(), s) == 0)
+            ans.push_back((int)(t.size() - s.size()));
+    }
+    for (int i = (int)t.size() - s.size() - 1; i >= 0; --i) {
+        input_hash = (input_hash - (ull)(t[i+s.size()] - 'A') * (ull)pow(26, s.size()-1) + prime) % prime;
+        input_hash = (input_hash * 26) % prime;
+        input_hash = (input_hash + (ull)(t[i] - 'A')) % prime;
+        // cout << "text hash: " << input_hash << std::endl;
+        if (pattern_hash == input_hash)
+            if (t.compare(i, s.size(), s) == 0)
+                ans.push_back((int)i);
+    }
     return ans;
 }
 
