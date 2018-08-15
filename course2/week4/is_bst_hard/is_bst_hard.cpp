f546f7ba1365101ba595c4eab61357e12731ee1a
fredtony <tony.schroeder@gmail.com>
2018-08-14 22:35:39 -0700

    Completed problems for in-order, pre=order, and post-order tree traversals, checking if a tree is a binary search tree proper, and checking if a binary search tree is a binary search tree with ties. No bugs detected in testing.

--- course2/week4/is_bst_hard/is_bst_hard.cpp
+++ course2/week4/is_bst_hard/is_bst_hard.cpp
@@ -8,32 +8,54 @@ using std::endl;
 using std::vector;
 
 struct Node {
-  int key;
-  int left;
-  int right;
+	long long key;
+	long long left;
+	long long right;
 
-  Node() : key(0), left(-1), right(-1) {}
-  Node(int key_, int left_, int right_) : key(key_), left(left_), right(right_) {}
+	Node() : key(0), left(-1), right(-1) {}
+	Node(long long key_, long long left_, long long right_) : key(key_), left(left_), right(right_) {}
 };
 
+bool IsBranchBinarySearchTree(const vector<Node>& tree, long long branch) {
+	if (tree[branch].left != -1) {
+		if (tree[tree[branch].left].key >= tree[branch].key or not IsBranchBinarySearchTree(tree, tree[branch].left))
+			return false;
+		long long max_check = tree[branch].left;
+		while (tree[max_check].right != -1) {
+			if (tree[tree[max_check].right].key >= tree[branch].key)
+				return false;
+			max_check = tree[max_check].right;
+		}
+	}
+	if (tree[branch].right != -1) {
+		if (tree[branch].key > tree[tree[branch].right].key or not IsBranchBinarySearchTree(tree, tree[branch].right))
+			return false;
+		long long min_check = tree[branch].right;
+		while (tree[min_check].left != -1) {
+			if (tree[tree[min_check].left].key < tree[branch].key)
+				return false;
+			min_check = tree[min_check].left;
+		}
+	}
+	return true;
+}
+
 bool IsBinarySearchTree(const vector<Node>& tree) {
-  // Implement correct algorithm here
-  return true;
+	return tree.size() == 0 or IsBranchBinarySearchTree(tree, 0);
 }
 
 int main() {
-  int nodes;
-  cin >> nodes;
-  vector<Node> tree;
-  for (int i = 0; i < nodes; ++i) {
-    int key, left, right;
-    cin >> key >> left >> right;
-    tree.push_back(Node(key, left, right));
-  }
-  if (IsBinarySearchTree(tree) {
-    cout << "CORRECT" << endl;
-  } else {
-    cout << "INCORRECT" << endl;
-  }
-  return 0;
+	int nodes;
+	cin >> nodes;
+	vector<Node> tree;
+	for (int i = 0; i < nodes; ++i) {
+		long long key, left, right;
+		cin >> key >> left >> right;
+		tree.push_back(Node(key, left, right));
+	}
+	if (IsBinarySearchTree(tree))
+		cout << "CORRECT" << endl;
+	else
+		cout << "INCORRECT" << endl;
+	return 0;
 }
--- course2/week4/tree_orders/tree-orders.cpp
+++ course2/week4/tree_orders/tree-orders.cpp
@@ -11,91 +11,113 @@ using std::cin;
 using std::cout;
 
 class TreeOrders {
-  int n;
-  vector <int> key;
-  vector <int> left;
-  vector <int> right;
+	int n;
+	vector <int> key;
+	vector <int> left;
+	vector <int> right;
 
 public:
-  void read() {
-    cin >> n;
-    key.resize(n);
-    left.resize(n);
-    right.resize(n);
-    for (int i = 0; i < n; i++) {
-      cin >> key[i] >> left[i] >> right[i];
-    }
-  }
+	void read() {
+		cin >> n;
+		key.resize(n);
+		left.resize(n);
+		right.resize(n);
+		for (int i = 0; i < n; i++) {
+			cin >> key[i] >> left[i] >> right[i];
+		}
+	}
 
+	void in_order_traversal(int idx, vector<int> &result) {
+		if (left[idx] != -1) {
+			in_order_traversal(left[idx], result);
+		}
+		result.push_back(key[idx]);
+		if (right[idx] != -1) {
+			in_order_traversal(right[idx], result);
+		}
+		return;
+	}
 
-  vector <int> in_order() {
-    vector<int> result;
-    // Finish the implementation
-    // You may need to add a new recursive method to do that
+	vector <int> in_order() {
+		vector<int> result;
+		in_order_traversal(0, result);
+		return result;
+	}
 
-    return result;
-  }
+	void pre_order_traversal(int idx, vector<int> &result) {
+		result.push_back(key[idx]);
+		if (left[idx] != -1) {
+			pre_order_traversal(left[idx], result);
+		}
+		if (right[idx] != -1) {
+			pre_order_traversal(right[idx], result);
+		}
+		return;
+	}
 
-  vector <int> pre_order() {
-    vector<int> result;    
-    // Finish the implementation
-    // You may need to add a new recursive method to do that
-    
-    return result;
-  }
+	vector <int> pre_order() {
+		vector<int> result;    
+		pre_order_traversal(0, result);
+		return result;
+	}
 
-  vector <int> post_order() {
-    vector<int> result;
-    // Finish the implementation
-    // You may need to add a new recursive method to do that
-    
-    return result;
-  }
+	void post_order_traversal(int idx, vector<int> &result) {
+		if (left[idx] != -1) {
+			post_order_traversal(left[idx], result);
+		}
+		if (right[idx] != -1) {
+			post_order_traversal(right[idx], result);
+		}
+		result.push_back(key[idx]);
+		return;
+	}
+
+	vector <int> post_order() {
+		vector<int> result;
+		post_order_traversal(0, result);
+		return result;
+	}
 };
 
 void print(vector <int> a) {
-  for (size_t i = 0; i < a.size(); i++) {
-    if (i > 0) {
-      cout << ' ';
-    }
-    cout << a[i];
-  }
-  cout << '\n';
+	for (size_t i = 0; i < a.size(); i++) {
+		if (i > 0) {
+			cout << ' ';
+		}
+		cout << a[i];
+	}
+	cout << '\n';
 }
 
 int main_with_large_stack_space() {
-  ios_base::sync_with_stdio(0);
-  TreeOrders t;
-  t.read();
-  print(t.in_order());
-  print(t.pre_order());
-  print(t.post_order());
-  return 0;
+	ios_base::sync_with_stdio(0);
+	TreeOrders t;
+	t.read();
+	print(t.in_order());
+	print(t.pre_order());
+	print(t.post_order());
+	return 0;
 }
 
-int main (int argc, char **argv)
-{
+int main (int argc, char **argv) {
 #if defined(__unix__) || defined(__APPLE__)
-  // Allow larger stack space
-  const rlim_t kStackSize = 16 * 1024 * 1024;   // min stack size = 16 MB
-  struct rlimit rl;
-  int result;
+	// Allow larger stack space
+	const rlim_t kStackSize = 16 * 1024 * 1024;   // min stack size = 16 MB
+	struct rlimit rl;
+	int result;
 
-  result = getrlimit(RLIMIT_STACK, &rl);
-  if (result == 0)
-  {
-      if (rl.rlim_cur < kStackSize)
-      {
-          rl.rlim_cur = kStackSize;
-          result = setrlimit(RLIMIT_STACK, &rl);
-          if (result != 0)
-          {
-              std::cerr << "setrlimit returned result = " << result << std::endl;
-          }
-      }
-  }
+	result = getrlimit(RLIMIT_STACK, &rl);
+	if (result == 0) {
+		if (rl.rlim_cur < kStackSize) {
+			rl.rlim_cur = kStackSize;
+			result = setrlimit(RLIMIT_STACK, &rl);
+			if (result != 0) {
+				std::cerr << "setrlimit returned result = " << result << std::endl;
+			}
+		}
+	}
 #endif
 
-  return main_with_large_stack_space();
+	return main_with_large_stack_space();
 }
 
