ffe4ae5eefeb004888b2a1b33f51520e447283ca
fredtony <tony.schroeder@gmail.com>
2018-08-15 00:49:28 -0700

    Completed set range sum data structure with online updates. No bugs detected.

--- course2/week4/set_range_sum/set_range_sum.cpp
+++ course2/week4/set_range_sum/set_range_sum.cpp
@@ -1,87 +1,93 @@
-#include <cstdio>
+// #include <cstdio>
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std:: string;
 
 // Splay tree implementation
 
 // Vertex of a splay tree
 struct Vertex {
-  int key;
-  // Sum of all the keys in the subtree - remember to update
-  // it after each operation that changes the tree.
-  long long sum;
-  Vertex* left;
-  Vertex* right;
-  Vertex* parent;
+	int key;
+	// Sum of all the keys in the subtree - remember to update
+	// it after each operation that changes the tree.
+	long long sum;
+	Vertex* left;
+	Vertex* right;
+	Vertex* parent;
 
-  Vertex(int key, long long sum, Vertex* left, Vertex* right, Vertex* parent) 
-  : key(key), sum(sum), left(left), right(right), parent(parent) {}
+	Vertex(int key, long long sum, Vertex* left, Vertex* right, Vertex* parent) 
+	: key(key), sum(sum), left(left), right(right), parent(parent) {}
 };
 
 void update(Vertex* v) {
-  if (v == NULL) return;
-  v->sum = v->key + (v->left != NULL ? v->left->sum : 0ll) + (v->right != NULL ? v->right->sum : 0ll);
-  if (v->left != NULL) {
-    v->left->parent = v;
-  }
-  if (v->right != NULL) {
-    v->right->parent = v;
-  }
+	if (v == NULL) return;
+	v->sum = v->key + (v->left != NULL ? v->left->sum : 0ll) + (v->right != NULL ? v->right->sum : 0ll);
+	if (v->left != NULL) {
+		v->left->parent = v;
+	}
+	if (v->right != NULL) {
+		v->right->parent = v;
+	}
 }
 
 void small_rotation(Vertex* v) {
-  Vertex* parent = v->parent;
-  if (parent == NULL) {
-    return;
-  }
-  Vertex* grandparent = v->parent->parent;
-  if (parent->left == v) {
-    Vertex* m = v->right;
-    v->right = parent;
-    parent->left = m;
-  } else {
-    Vertex* m = v->left;
-    v->left = parent;
-    parent->right = m;
-  }
-  update(parent);
-  update(v);
-  v->parent = grandparent;
-  if (grandparent != NULL) {
-    if (grandparent->left == parent) {
-      grandparent->left = v;
-    } else {
-      grandparent->right = v;
-    }
-  }
+	Vertex* parent = v->parent;
+	if (parent == NULL) {
+		return;
+	}
+	Vertex* grandparent = v->parent->parent;
+	if (parent->left == v) {
+		Vertex* m = v->right;
+		v->right = parent;
+		parent->left = m;
+	} else {
+		Vertex* m = v->left;
+		v->left = parent;
+		parent->right = m;
+	}
+	update(parent);
+	update(v);
+	v->parent = grandparent;
+	if (grandparent != NULL) {
+		if (grandparent->left == parent) {
+			grandparent->left = v;
+		} else {
+			grandparent->right = v;
+		}
+	}
 }
 
 void big_rotation(Vertex* v) {
-  if (v->parent->left == v && v->parent->parent->left == v->parent) {
-    // Zig-zig
-    small_rotation(v->parent);
-    small_rotation(v);
-  } else if (v->parent->right == v && v->parent->parent->right == v->parent) {
-    // Zig-zig
-    small_rotation(v->parent);
-    small_rotation(v);
-  } else {
-    // Zig-zag
-    small_rotation(v);
-    small_rotation(v);
-  }  
+	if (v->parent->left == v && v->parent->parent->left == v->parent) {
+		// Zig-zig
+		small_rotation(v->parent);
+		small_rotation(v);
+	} else if (v->parent->right == v && v->parent->parent->right == v->parent) {
+		// Zig-zig
+		small_rotation(v->parent);
+		small_rotation(v);
+	} else {
+		// Zig-zag
+		small_rotation(v);
+		small_rotation(v);
+	}
 }
 
 // Makes splay of the given vertex and makes
 // it the new root.
 void splay(Vertex*& root, Vertex* v) {
-  if (v == NULL) return;
-  while (v->parent != NULL) {
-    if (v->parent->parent == NULL) {
-      small_rotation(v);
-      break;
-    }
-    big_rotation(v);
-  }
-  root = v;
+	if (v == NULL) return;
+	while (v->parent != NULL) {
+		if (v->parent->parent == NULL) {
+			small_rotation(v);
+			break;
+		}
+		big_rotation(v);
+	}
+	root = v;
 }
 
 // Searches for the given key in the tree with the given root
@@ -92,54 +98,54 @@ void splay(Vertex*& root, Vertex* v) {
 // If the key is bigger than all keys in the tree, 
 // returns NULL.
 Vertex* find(Vertex*& root, int key) {
-  Vertex* v = root;
-  Vertex* last = root;
-  Vertex* next = NULL;
-  while (v != NULL) {
-    if (v->key >= key && (next == NULL || v->key < next->key)) {
-      next = v;
-    }
-    last = v;
-    if (v->key == key) {
-      break;      
-    }
-    if (v->key < key) {
-      v = v->right;
-    } else {
-      v = v->left;
-    }
-  }
-  splay(root, last);
-  return next;
+	Vertex* v = root;
+	Vertex* last = root;
+	Vertex* next = NULL;
+	while (v != NULL) {
+		if (v->key >= key && (next == NULL || v->key < next->key)) {
+			next = v;
+		}
+		last = v;
+		if (v->key == key) {
+			break;      
+		}
+		if (v->key < key) {
+			v = v->right;
+		} else {
+			v = v->left;
+		}
+	}
+	splay(root, last);
+	return next;
 }
 
 void split(Vertex* root, int key, Vertex*& left, Vertex*& right) {
-  right = find(root, key);
-  splay(root, right);
-  if (right == NULL) {
-    left = root;
-    return;
-  }
-  left = right->left;
-  right->left = NULL;
-  if (left != NULL) {
-    left->parent = NULL;
-  }
-  update(left);
-  update(right);
+	right = find(root, key);
+	splay(root, right);
+	if (right == NULL) {
+		left = root;
+		return;
+	}
+	left = right->left;
+	right->left = NULL;
+	if (left != NULL) {
+		left->parent = NULL;
+	}
+	update(left);
+	update(right);
 }
 
 Vertex* merge(Vertex* left, Vertex* right) {
-  if (left == NULL) return right;
-  if (right == NULL) return left;
-  Vertex* min_right = right;
-  while (min_right->left != NULL) {
-    min_right = min_right->left;
-  }
-  splay(right, min_right);
-  right->left = left;
-  update(right);
-  return right;
+	if (left == NULL) return right;
+	if (right == NULL) return left;
+	Vertex* min_right = right;
+	while (min_right->left != NULL) {
+		min_right = min_right->left;
+	}
+	splay(right, min_right);
+	right->left = left;
+	update(right);
+	return right;
 }
 
 // Code that uses splay tree to solve the problem
@@ -147,73 +153,89 @@ Vertex* merge(Vertex* left, Vertex* right) {
 Vertex* root = NULL;
 
 void insert(int x) {
-  Vertex* left = NULL;
-  Vertex* right = NULL;
-  Vertex* new_vertex = NULL;  
-  split(root, x, left, right);
-  if (right == NULL || right->key != x) {
-    new_vertex = new Vertex(x, x, NULL, NULL, NULL);
-  }
-  root = merge(merge(left, new_vertex), right);
+	Vertex* left = NULL;
+	Vertex* right = NULL;
+	Vertex* new_vertex = NULL;  
+	split(root, x, left, right);
+	if (right == NULL || right->key != x) {
+		new_vertex = new Vertex(x, x, NULL, NULL, NULL);
+	}
+	root = merge(merge(left, new_vertex), right);
 }
 
 void erase(int x) {                   
-  // Implement erase yourself
-
+	Vertex* left = NULL;
+	Vertex* right = NULL;
+	Vertex* found = find(root, x);
+	if (found != NULL && found->key == x) {
+		split(root, x, left, right);
+		if (right != NULL) {
+		    root = merge(merge(left, right->left), right->right);
+		}
+		else {
+		    root = left;
+		}
+	}
+	return;
 }
 
-bool find(int x) {  
-  // Implement find yourself
-
-  return false;
+bool find(int x) {
+	Vertex* found = find(root, x);
+	if (found != NULL && found->key == x) {
+		return true;
+	}
+	return false;
 }
 
 long long sum(int from, int to) {
-  Vertex* left = NULL;
-  Vertex* middle = NULL;
-  Vertex* right = NULL;
-  split(root, from, left, middle);
-  split(middle, to + 1, middle, right);
-  long long ans = 0;
-  // Complete the implementation of sum
-  
-  return ans;  
+	Vertex* left = NULL;
+	Vertex* middle = NULL;
+	Vertex* right = NULL;
+	split(root, from, left, middle);
+	split(middle, to + 1, middle, right);
+	long long ans = 0;
+	if (middle != NULL) {
+	    ans = middle->sum;
+	}
+	root = merge(merge(left, middle), right);
+	return ans;  
 }
 
 const int MODULO = 1000000001;
 
-int main(){
-  int n;
-  scanf("%d", &n);
-  int last_sum_result = 0;
-  for (int i = 0; i < n; i++) {
-    char buffer[10];
-    scanf("%s", buffer);
-    char type = buffer[0];
-    switch (type) {
-      case '+' : {
-        int x;
-        scanf("%d", &x);
-        insert((x + last_sum_result) % MODULO);
-      } break;
-      case '-' : {
-        int x;
-        scanf("%d", &x);
-        erase((x + last_sum_result) % MODULO);
-      } break;            
-      case '?' : {
-        int x;
-        scanf("%d", &x);
-        printf(find((x + last_sum_result) % MODULO) ? "Found\n" : "Not found\n");
-      } break;
-      case 's' : {
-        int l, r;
-        scanf("%d %d", &l, &r);
-        long long res = sum((l + last_sum_result) % MODULO, (r + last_sum_result) % MODULO);
-        printf("%lld\n", res);
-        last_sum_result = int(res % MODULO);
-      }
-    }
-  }
-  return 0;
+int main() {
+	int n;
+	cin >> n;
+	int last_sum_result = 0;
+	for (int i = 0; i < n; i++) {
+		char type;
+		cin >> type;
+		switch (type) {
+			case '+' : {
+				int x;
+				cin >> x;
+				insert((x + last_sum_result) % MODULO);
+			} break;
+			case '-' : {
+				int x;
+				cin >> x;
+				erase((x + last_sum_result) % MODULO);
+			} break;            
+			case '?' : {
+				int x;
+				cin >> x;
+				string find_result;
+				find_result = find((x + last_sum_result) % MODULO) ? "Found" : "Not found";
+				cout << find_result <<std::endl;
+			} break;
+			case 's' : {
+				int l, r;
+				cin >> l >> r;
+				long long res = sum((l + last_sum_result) % MODULO, (r + last_sum_result) % MODULO);
+				cout << res << std::endl;
+				last_sum_result = int(res % MODULO);
+			}
+		}
+	}
+	return 0;
 }
