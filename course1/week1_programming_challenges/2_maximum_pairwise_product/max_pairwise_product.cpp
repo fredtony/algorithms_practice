eea841f801869af0358282dec15c93ff15c7b51a
fredtony <tony.schroeder@gmail.com>
2018-08-16 11:15:32 -0700

    Fixed syntax in max pairwise product. Did not save changes in original version.

--- course1/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.cpp
+++ course1/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.cpp
@@ -8,12 +8,16 @@ using namespace std;
 int MaxPairwiseProduct(const vector<long long>& numbers) {
     int n = numbers.size();
 
-    long long num1 = max(numbers[0], numbers[1])
-    long long num2 = min(numbers[0], numbers[1])
+    long long num1 = max(numbers[0], numbers[1]);
+    long long num2 = min(numbers[0], numbers[1]);
 
     for (int i = 2; i < n; ++i) {
-        if (numbers[i] > num1) { num2 = num1; num1 = numbers[i]; }
-        else if (numbers[i] > num2) num2 = numbers[i];
+        if (numbers[i] > num1) {
+        	num2 = num1; num1 = numbers[i];
+        }
+        else if (numbers[i] > num2) {
+        	num2 = numbers[i];
+        }
     }
     return num1 * num2;
 }
