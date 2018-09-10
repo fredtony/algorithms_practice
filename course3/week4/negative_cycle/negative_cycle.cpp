0a583a548f28e94e713304b50b3a13ba083a2067
fredtony <tony.schroeder@gmail.com>
2018-09-10 09:33:18 -0700

    Completed shortest path which can include negative weights and negative cycles using Bellman-Ford. It starts at a node and detects if other nodes are detectable, whether they are affected by infinite arbitrage, and if not, their shortest distance.

--- course3/week4/negative_cycle/negative_cycle.cpp
+++ course3/week4/negative_cycle/negative_cycle.cpp
@@ -3,21 +3,46 @@
 
 using std::vector;
 
+#define INFINITY 5000000
+
+void relax(vector<int> &prev, vector<int> &dist, int u, int v, int weight) {
+	if (dist[v] > dist[u] + weight) {
+		dist[v] = dist[u] + weight;
+		prev[v] = u;
+	}
+}
+
 int negative_cycle(vector<vector<int> > &adj, vector<vector<int> > &cost) {
-  //write your code here
-  return 0;
+	vector<int> prev(adj.size(), -1);
+	vector<int> dist(adj.size(), INFINITY);
+	dist[0] = 0;
+	for (int i = 0; i < adj.size() - 1; ++i) {
+		for (int j = 0; j < adj.size(); ++j) {
+			for (int k = 0; k < adj[j].size(); ++k) {
+				relax(prev, dist, j, adj[j][k], cost[j][k]);
+			}
+		}
+	}
+	for (int j = 0; j < adj.size(); ++j) {
+		for (int k = 0; k < adj[j].size(); ++k) {
+			if (dist[adj[j][k]] > dist[j] + cost[j][k]) {
+				return 1;
+			}
+		}
+	}
+	return 0;
 }
 
 int main() {
-  int n, m;
-  std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
-  vector<vector<int> > cost(n, vector<int>());
-  for (int i = 0; i < m; i++) {
-    int x, y, w;
-    std::cin >> x >> y >> w;
-    adj[x - 1].push_back(y - 1);
-    cost[x - 1].push_back(w);
-  }
-  std::cout << negative_cycle(adj, cost);
+	int n, m;
+	std::cin >> n >> m;
+	vector<vector<int> > adj(n, vector<int>());
+	vector<vector<int> > cost(n, vector<int>());
+	for (int i = 0; i < m; ++i) {
+		int x, y, w;
+		std::cin >> x >> y >> w;
+		adj[x - 1].push_back(y - 1);
+		cost[x - 1].push_back(w);
+	}
+	std::cout << negative_cycle(adj, cost);
 }
--- course3/week4/shortest_paths/shortest_paths.cpp
+++ course3/week4/shortest_paths/shortest_paths.cpp
@@ -8,34 +8,75 @@ using std::queue;
 using std::pair;
 using std::priority_queue;
 
+#define INF std::numeric_limits<long long>::max()
+
+void relax(vector<int> &prev, vector<long long> &dist, int u, int v, int weight) {
+	if (dist[u] != INF && dist[v] > dist[u] + weight) {
+		dist[v] = dist[u] + weight;
+		prev[v] = u;
+	}
+}
+
 void shortest_paths(vector<vector<int> > &adj, vector<vector<int> > &cost, int s, vector<long long> &distance, vector<int> &reachable, vector<int> &shortest) {
-  //write your code here
+	vector<int> prev(adj.size(), -1);
+	distance[s] = 0;
+	for (int i = 0; i < adj.size() - 1; ++i) {
+		for (int j = 0; j < adj.size(); ++j) {
+			for (int k = 0; k < adj[j].size(); ++k) {
+				relax(prev, distance, j, adj[j][k], cost[j][k]);
+			}
+		}
+	}
+	queue<int> in_neg_cycles;
+	for (int j = 0; j < adj.size(); ++j) {
+		if (distance[j] < std::numeric_limits<long long>:: max()) {
+			reachable[j] = 1;
+		}
+		for (int k = 0; k < adj[j].size(); ++k) {
+			if (distance[j] != INF && distance[adj[j][k]] > distance[j] + cost[j][k]) {
+				in_neg_cycles.push(adj[j][k]);
+			}
+		}
+	}
+	int curr_vertex, next_vertex;
+	while (!in_neg_cycles.empty()) {
+		curr_vertex = in_neg_cycles.front();
+		shortest[curr_vertex] = 0;
+		in_neg_cycles.pop();
+		for (int i = 0; i < adj[curr_vertex].size(); ++i) {
+			next_vertex = adj[curr_vertex][i];
+			if (shortest[next_vertex]) {
+				shortest[next_vertex] = 0;
+				in_neg_cycles.push(next_vertex);
+			}
+		}
+	}
 }
 
 int main() {
-  int n, m, s;
-  std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
-  vector<vector<int> > cost(n, vector<int>());
-  for (int i = 0; i < m; i++) {
-    int x, y, w;
-    std::cin >> x >> y >> w;
-    adj[x - 1].push_back(y - 1);
-    cost[x - 1].push_back(w);
-  }
-  std::cin >> s;
-  s--;
-  vector<long long> distance(n, std::numeric_limits<long long>::max());
-  vector<int> reachable(n, 0);
-  vector<int> shortest(n, 1);
-  shortest_paths(adj, cost, s, distance, reachable, shortest);
-  for (int i = 0; i < n; i++) {
-    if (!reachable[i]) {
-      std::cout << "*\n";
-    } else if (!shortest[i]) {
-      std::cout << "-\n";
-    } else {
-      std::cout << distance[i] << "\n";
-    }
-  }
+	int n, m, s;
+	std::cin >> n >> m;
+	vector<vector<int> > adj(n, vector<int>());
+	vector<vector<int> > cost(n, vector<int>());
+	for (int i = 0; i < m; i++) {
+		int x, y, w;
+		std::cin >> x >> y >> w;
+		adj[x - 1].push_back(y - 1);
+		cost[x - 1].push_back(w);
+	}
+	std::cin >> s;
+	s--;
+	vector<long long> distance(n, INF);
+	vector<int> reachable(n, 0);
+	vector<int> shortest(n, 1);
+	shortest_paths(adj, cost, s, distance, reachable, shortest);
+	for (int i = 0; i < n; i++) {
+		if (!reachable[i]) {
+			std::cout << "*\n";
+		} else if (!shortest[i]) {
+			std::cout << "-\n";
+		} else {
+			std::cout << distance[i] << "\n";
+		}
+	}
 }
