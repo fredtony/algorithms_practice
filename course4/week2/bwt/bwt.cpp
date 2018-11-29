e1654d4e0db55e33482ed2dea6ed84c0691ce24f
fredtony <tony.schroeder@gmail.com>
2018-11-29 06:45:04 -0800

    Completed exercises to create BWT array and invert a BWT array

--- course4/week2/bwt/bwt.cpp
+++ course4/week2/bwt/bwt.cpp
@@ -10,16 +10,38 @@ using std::string;
 using std::vector;
 
 string BWT(const string& text) {
-  string result = "";
+	string result = "";
+	vector<string> strings;
+	string shortString = text;
+	int l = text.size() - 1;
+	strings.push_back(shortString);
 
-  // write your code here
+	for (int i = 1; i < text.size(); ++i) {
+		shortString = text.substr(i, text.size() - i) + text.substr(0, i);
+		// cout << shortString << endl;
+		strings.push_back(shortString);
+	}
 
-  return result;
+	// for (int i = 0; i < strings.size(); ++i) {
+	// 	cout << strings[i] << endl;
+	// }
+
+	std::sort(strings.begin(), strings.end());
+
+	// for (int i = 0; i < strings.size(); ++i) {
+	// 	cout << strings[i] << endl;
+	// }
+
+	for (int i = 0; i < strings.size(); ++i) {
+		result = result + strings[i][l];
+	}
+
+	return result;
 }
 
 int main() {
-  string text;
-  cin >> text;
-  cout << BWT(text) << endl;
-  return 0;
+	string text;
+	cin >> text;
+	cout << BWT(text) << endl;
+	return 0;
 }
--- course4/week2/bwtinverse/bwtinverse.cpp
+++ course4/week2/bwtinverse/bwtinverse.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cassert>
 
 using std::cin;
 using std::cout;
@@ -9,17 +10,76 @@ using std::endl;
 using std::string;
 using std::vector;
 
+int const Letters = 5;
+
+int idx(char letter) {
+	switch (letter) {
+		case '$': return 0; break;
+		case 'A': return 1; break;
+		case 'C': return 2; break;
+		case 'G': return 3; break;
+		case 'T': return 4; break;
+		default: assert(false); return -1;
+	}
+}
+
+vector<vector<int>> createFMIndex(const string& bwt) {
+	vector<vector<int>> result;
+	vector<int> row (Letters, 0);
+
+	result.push_back(row);
+	for (int i = 0; i < bwt.size(); ++i) {
+		++row[idx(bwt[i])];
+		result.push_back(row);
+	}
+	int count = 0;
+	for (int i = 0; i < Letters; ++i) {
+		row[i] = count;
+		count += result[bwt.size()][i];
+	}
+	result.push_back(row);
+	return result;
+}
+
 string InverseBWT(const string& bwt) {
-  string text = "";
+	char nextChar = bwt[0];
+	string text = bwt;
+	text[text.size()-1] = '$';
+	text[text.size()-2] = nextChar;
+	// cout << "Text is: " << text << endl;
+	vector<vector<int>> fmIndex = createFMIndex(bwt);
 
-  // write your code here
+	// for (int i = 0; i < fmIndex.size(); ++i) {
+	// 	for (int j = 0; j < fmIndex[i].size(); ++j) {
+	// 		cout << fmIndex[i][j] << " ";
+	// 	}
+	// 	cout << endl;
+	// }
+	// cout << endl;
 
-  return text;
+	vector<int> tracker = fmIndex[fmIndex.size()-1];
+	int c = idx(nextChar);
+	int pred = tracker[c] + fmIndex[0][c];
+	int count = text.size() - 3;
+	while (pred != 0) {
+		nextChar = bwt[pred];
+		c = idx(nextChar);
+		pred = tracker[c] + fmIndex[pred][c];
+		// if (nextChar != '$') {
+		if (count >= 0) {
+			text[count--] = nextChar;
+		}
+		
+		// if (count >= -1)
+		// 	cout << "Text is: " << text << endl;
+	}
+
+	return text;
 }
 
 int main() {
-  string bwt;
-  cin >> bwt;
-  cout << InverseBWT(bwt) << endl;
-  return 0;
+	string bwt;
+	cin >> bwt;
+	cout << InverseBWT(bwt) << endl;
+	return 0;
 }
