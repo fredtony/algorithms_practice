2da866c5fbc04926db6bf4c7a3ebeeb9bff5c0f5
fredtony <tony.schroeder@gmail.com>
2018-12-01 07:57:39 -0800

    Completed pattern matching in a BWT array and computing suffix arrays.

--- course4/week2/bwmatching/bwmatching.cpp
+++ course4/week2/bwmatching/bwmatching.cpp
@@ -7,10 +7,16 @@
 #include <vector>
 
 using std::cin;
-using std::istringstream;
+using std::cout;
+using std::endl;
 using std::map;
 using std::string;
 using std::vector;
+
+const bool debug = false;
+const int Letters = 5;
+const string alph = "$ACGT";
+map<char, int> idx = {{'$', 0}, {'A', 1}, {'C', 2}, {'G', 3}, {'T', 4}};
 
 // Preprocess the Burrows-Wheeler Transform bwt of some text
 // and compute as a result:
@@ -23,7 +29,24 @@ using std::vector;
 void PreprocessBWT(const string& bwt, 
                    map<char, int>& starts, 
                    map<char, vector<int> >& occ_count_before) {
-  // Implement this function yourself
+	vector<int> newVect (1, 0);
+	for (int i = 0; i < Letters; ++i) {
+		occ_count_before[alph[i]] = newVect;
+	}
+	for (int i = 0; i < bwt.size(); ++i) {
+		for (char c : alph) {
+			if (bwt[i] == c) {
+				occ_count_before[c].push_back(occ_count_before[c].back() + 1);
+			} else {
+				occ_count_before[c].push_back(occ_count_before[c].back());
+			}
+		}
+	}
+	int count = 0;
+	for (char c: alph) {
+		starts[c] = count;
+		count += occ_count_before[c].back();
+	}
 }
 
 // Compute the number of occurrences of string pattern in the text
@@ -31,35 +54,64 @@ void PreprocessBWT(const string& bwt,
 // information we get from the preprocessing stage - starts and occ_counts_before.
 int CountOccurrences(const string& pattern, 
                      const string& bwt, 
-                     const map<char, int>& starts, 
-                     const map<char, vector<int> >& occ_count_before) {
-  // Implement this function yourself
-  return 0;
+                     const map<char,int>& starts, 
+                     const map<char,vector<int>>& occ_count_before) {
+	int top = 0, bottom = bwt.size() - 1;
+	string patt = pattern;
+	char c;
+	while (top <= bottom) {
+		if (patt.empty()) {
+			return bottom - top + 1;
+		} else {
+			c = patt.back();
+			patt.pop_back();
+			if (occ_count_before.at(c)[top] != occ_count_before.at(c)[bottom+1]) {
+				top = starts.at(c) + occ_count_before.at(c)[top];
+				bottom = starts.at(c) + occ_count_before.at(c)[bottom + 1] - 1;
+			} else {
+				return 0;
+			}
+		}
+	}
+	return -1;
 }
      
 
 int main() {
-  string bwt;
-  cin >> bwt;
-  int pattern_count;
-  cin >> pattern_count;
-  // Start of each character in the sorted list of characters of bwt,
-  // see the description in the comment about function PreprocessBWT
-  map<char, int> starts;
-  // Occurrence counts for each character and each position in bwt,
-  // see the description in the comment about function PreprocessBWT
-  map<char, vector<int> > occ_count_before;
-  // Preprocess the BWT once to get starts and occ_count_before.
-  // For each pattern, we will then use these precomputed values and
-  // spend only O(|pattern|) to find all occurrences of the pattern
-  // in the text instead of O(|pattern| + |text|).
-  PreprocessBWT(bwt, starts, occ_count_before);
-  for (int pi = 0; pi < pattern_count; ++pi) {
-    string pattern;
-    cin >> pattern;
-    int occ_count = CountOccurrences(pattern, bwt, starts, occ_count_before);
-    printf("%d ", occ_count);
-  }
-  printf("\n");
-  return 0;
+	string bwt;
+	cin >> bwt;
+	int pattern_count;
+	cin >> pattern_count;
+	// Start of each character in the sorted list of characters of bwt,
+	// see the description in the comment about function PreprocessBWT
+	map<char, int> starts;
+	// Occurrence counts for each character and each position in bwt,
+	// see the description in the comment about function PreprocessBWT
+	map<char, vector<int> > occ_count_before;
+	// Preprocess the BWT once to get starts and occ_count_before.
+	// For each pattern, we will then use these precomputed values and
+	// spend only O(|pattern|) to find all occurrences of the pattern
+	// in the text instead of O(|pattern| + |text|).
+	PreprocessBWT(bwt, starts, occ_count_before);
+	if (debug) {
+		for (char c: alph) {
+			cout << c << ":" << starts[c] << " ";
+		} cout << endl << endl;
+		for (char c: alph) cout << c << " "; cout << endl;
+		for (int i = 0; i < occ_count_before['$'].size(); ++i) {
+			for (char c: alph) {
+				cout << occ_count_before[c][i] << " ";
+			} cout << endl;
+		} cout << endl;
+	}
+
+	for (int pi = 0; pi < pattern_count; ++pi) {
+		string pattern;
+		cin >> pattern;
+		int occ_count = CountOccurrences(pattern, bwt, starts, occ_count_before);
+		if (occ_count == -1) cout << "ERROR";
+		cout << occ_count << " ";
+	}
+	cout << endl;
+	return 0;
 }
--- course4/week2/suffix_array/suffix_array.cpp
+++ course4/week2/suffix_array/suffix_array.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <utility>
+#include <cassert>
 
 using std::cin;
 using std::cout;
@@ -11,6 +12,62 @@ using std::make_pair;
 using std::pair;
 using std::string;
 using std::vector;
+using std::sort;
+
+bool const debug = false;
+// int const Letters = 5;
+
+// int idx(char letter) {
+// 	switch (letter) {
+// 		case '$': return 0; break;
+// 		case 'A': return 1; break;
+// 		case 'C': return 2; break;
+// 		case 'G': return 3; break;
+// 		case 'T': return 4; break;
+// 		default: assert(false); return -1;
+// 	}
+// }
+
+// vector<vector<int>> createFMIndex(const string& bwt) {
+// 	vector<vector<int>> result;
+// 	vector<int> row (Letters, 0);
+
+// 	result.push_back(row);
+// 	for (int i = 0; i < bwt.size(); ++i) {
+// 		++row[idx(bwt[i])];
+// 		result.push_back(row);
+// 	}
+// 	int count = 0;
+// 	for (int i = 0; i < Letters; ++i) {
+// 		row[i] = count;
+// 		count += result[bwt.size()][i];
+// 	}
+// 	result.push_back(row);
+// 	return result;
+// }
+
+// string InverseBWT(const string& bwt) {
+// 	char nextChar = bwt[0];
+// 	string text = bwt;
+// 	text[text.size()-1] = '$';
+// 	text[text.size()-2] = nextChar;
+// 	vector<vector<int>> fmIndex = createFMIndex(bwt);
+
+// 	vector<int> tracker = fmIndex[fmIndex.size()-1];
+// 	int c = idx(nextChar);
+// 	int pred = tracker[c] + fmIndex[0][c];
+// 	int count = text.size() - 3;
+// 	while (pred != 0) {
+// 		nextChar = bwt[pred];
+// 		c = idx(nextChar);
+// 		pred = tracker[c] + fmIndex[pred][c];
+// 		if (count >= 0) {
+// 			text[count--] = nextChar;
+// 		}
+// 	}
+
+// 	return text;
+// }
 
 // Build suffix array of the string text and
 // return a vector result of the same length as the text
@@ -18,18 +75,33 @@ using std::vector;
 // in text where the i-th lexicographically smallest
 // suffix of text starts.
 vector<int> BuildSuffixArray(const string& text) {
-  vector<int> result;
-  // Implement this function yourself
-  return result;
+	vector<int> result;
+	vector<string> suffix;
+	// string realText = InverseBWT(text);
+	// if (debug) cout << realText << endl;
+	// for (int i = 0; i < text.size(); ++i) {
+	// 	suffix.push_back(realText.substr(i, text.size() - i));
+	// }
+	// sort(suffix.begin(), suffix.end());
+	for (int i = 0; i < text.size(); ++i) {
+		suffix.push_back(text.substr(i, text.size() - i));
+	}
+	sort(suffix.begin(), suffix.end());
+	for (int i = 0; i < suffix.size(); ++i) {
+		if (debug) cout << suffix[i] << endl;
+		result.push_back(text.size() - suffix[i].size());
+	} if (debug) cout << endl << endl;
+
+	return result;
 }
 
 int main() {
-  string text;
-  cin >> text;
-  vector<int> suffix_array = BuildSuffixArray(text);
-  for (int i = 0; i < suffix_array.size(); ++i) {
-    cout << suffix_array[i] << ' ';
-  }
-  cout << endl;
-  return 0;
+	string text;
+	cin >> text;
+	vector<int> suffix_array = BuildSuffixArray(text);
+	for (int i = 0; i < suffix_array.size(); ++i) {
+		cout << suffix_array[i] << ' ';
+	}
+	cout << endl;
+	return 0;
 }
